838a97ca7c44ed052f5c7e4aed4e83d33155c6b8
Clinton Olson <clint.olson2@gmail.com>
2015-02-20 23:26:01 -0800

    finished basic linear regression. still needs some work for the gradient descent

--- LinReg.py
+++ LinReg.py
@@ -16,7 +16,7 @@ class LinReg:
         self.weights = None
         
 
-    def train(self, data, targets, eta = 0.2, lamb = 0, useNormEq = True):
+    def train(self, data, targets, eta = 0.2, lamb = 0, useNormEq = True, iters = 100):
         '''
         Inputs:
             - data: samples in rows, vars in columns
@@ -26,24 +26,57 @@ class LinReg:
             - useNormEq: flag for using the normal equation to solve directly
                          defaults to True
         '''
-        self.data = data
-        self.targets = targets
+        self.data = np.insert(data,0,1,axis=1)
+        self.targets = targets.T
         self.nsamps, self.nvars = data.shape
 
         if useNormEq:
             # try to invert but revert to pseudo-inverse if necessary
             try:
-                xInv = np.linalg.inv(np.dot(data.T,data))
+                xInv = np.linalg.inv(np.dot(self.data.T,self.data))
 
             except LinAlgError:
-                xInv = np.linalg.pinv(np.dot(data.T,data))
+                xInv = np.linalg.pinv(np.dot(self.data.T,self.data))
 
-            self.weights = np.dot(np.dot(xInv, data.T),targets.T)
+            self.weights = np.dot(np.dot(xInv, self.data.T),targets.T)
             
         else:
-            # do the gradient descent thing
-            x = 1 
+            # perform gradient descent
+            self.gradDescent(eta,lamb,iters)
 
 
+    def gradDescent(self, eta, lamb, iters):
+        '''
+            This version of gradient descent is not normalized
+        ''' 
         # initialize weights to small positive and negative values
-        #self.weights = np.random.rand(self.nvars) * -0.1 - 0.05
+        self.weights = np.random.rand(1,self.nvars+1).T * -0.1 - 0.05
+
+        currCost = self.costF()
+
+        for i in range(iters):
+            h = np.dot(self.data,self.weights).T - self.targets
+            errTerm = (eta/self.nsamps) * np.dot(h,self.data).T
+            self.weights = self.weights - errTerm
+
+
+    def costF(self):
+        # compute the cost of the current weights
+        # currently using the SSE cost measure
+        return (1.0/(2*self.nsamps))*np.sum((np.dot(self.data,self.weights) - self.targets) ** 2)
+        
+
+
+
+    def predict(self, data):
+        '''
+        Inputs:
+            - data: samples in rows
+        Outputs:
+            - an array of predicted values based on trained model
+        '''
+        if self.weights is None:
+            print 'Model weights have not been trained'
+        else:
+            testD = np.insert(data,0,1,axis=1)
+            return np.dot(testD,self.weights)
--- __init__.py
+++ __init__.py
@@ -1,1 +1,2 @@
 # imports
+from LinReg import *
