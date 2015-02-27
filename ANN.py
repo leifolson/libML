ea0375405f3430201be8aec5210f34b8fa7d2f26
Clinton Olson <clint.olson2@gmail.com>
2015-02-26 22:43:17 -0800

    added some skeleton functions for the artificial neural net

--- ANN.py
+++ ANN.py
@@ -4,3 +4,20 @@ Email: clint.olson2@gmail.com
 
 Implements an Artificial Neural Network Class
 '''
+
+class ANN:
+
+    def train():
+
+
+        return # no implementation yet
+
+
+    def forwardProp():
+        # TODO: implement
+        return
+
+
+    def backProp():
+        # TODO: implement
+        return
