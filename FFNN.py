b4706bf189c1213cfc3c4c229dd327a4e36e698e
Clinton Olson <clint.olson2@gmail.com>
2015-08-31 22:55:20 -0700

    fixed up some errors in my weight updates
    also added random restarts to try different random weight initializations for better overall results
    
    the train method now stores the best set of weights for all initializations

--- FFNN.py
+++ FFNN.py
@@ -11,7 +11,7 @@ import numpy as np
 
 class FFNN:
     
-    def __init__(self, data, targets, layers, hiddenType = 'logistic', outType = 'logistic'):
+    def __init__(self, data, targets, layers, hiddenType = 'logistic', outType = 'logistic', bias = -1):
         '''
         Inputs:
             - data: numpy array of training data, samples in rows
@@ -29,28 +29,41 @@ class FFNN:
         self.layers = layers
         self.weights = self._initWeights(layers)
         self.bestWeights = self.weights
+        self.bias = bias
         
 
-    def train(self, epochs, learningRate):
+    def train(self, epochs, learningRate, randomRestarts = 5):
         # TODO: implement
+        bestError = 99999
+        bestWeights = self.weights
         
-        for i in range(epochs):
-            acts = self._forwardProp()
+        for i in range(randomRestarts):
             
-            # update weights
-            self._backProp(acts, learningRate)
+            self.weights = self._initWeights(self.layers)
             
-            print self.weights
+            for i in range(epochs):
+                acts = self._forwardProp()
+                
+                # update weights
+                self._backProp(acts, learningRate)
+                
+                error = sum((self.targets - acts[len(self.layers) - 2])**2)
+                print 'targets: ' , self.targets
+                print 'acts: ' , acts[len(self.layers) - 2]
+                print 'errors: ' , error
+                
+                if error < bestError:
+                    bestError = error
+                    bestWeights = self.weights.copy()
+                
+                idx = range(self.data.shape[0])
+                np.random.shuffle(idx)
+                self.data = self.data[idx,:]
+                self.targets = self.targets[idx, :]
             
-            print 'targets: ' , self.targets
-            print 'acts: ' , acts[len(self.layers) - 2]
-            print 'errors: ' , sum((self.targets - acts[len(self.layers) - 2])**2)
-            
-            idx = range(self.data.shape[0])
-            np.random.shuffle(idx)
-            self.data = self.data[idx,:]
-            self.targets = self.targets[idx, :]
-            
+        self.weights = bestWeights.copy()
+        print 'best error: ', bestError
+        
         return 
 
     def predict(self):
@@ -67,29 +80,31 @@ class FFNN:
         activations = {}
         
         numLayers = len(self.layers)
+        outputLayer = numLayers - 2
         
         # get activation function type for hidden layers
         h = self._getActivationFunc(self.hiddenType)
         
         # add bias to input
-        trainEx = np.insert(self.data, 0, 1, axis = 1)
+        trainEx = np.insert(self.data.copy(), 0, self.bias, axis = 1)
         
         # compute input to first layer
         neuralInput = np.dot(trainEx, self.weights[0].T)
         
         for i in range(numLayers - 1):
             # check if we need to get the output layer activation function type
-            if(i == numLayers - 2):
+            if(i == outputLayer):
                 h = self._getActivationFunc(self.outType)
             
             # compute activations and store them
             acts = h(neuralInput)    
-            activations.update({i : acts})
 
             # add bias node for next input and compute next input if needed
-            if(i != numLayers - 2):
-                acts = np.insert(acts, 0, 1, axis = 1)
+            if(i < outputLayer):
+                acts = np.insert(acts, 0, self.bias, axis = 1)
                 neuralInput = np.dot(acts, self.weights[i + 1].T)
+                
+            activations.update({i : acts})
         
         return activations
         
@@ -103,13 +118,13 @@ class FFNN:
         '''
         
         numLayers = len(self.layers)
+        outputLayer = numLayers - 2
 
         # start at the output and propagate back
         deltas = {}
         
         # compute output errors
         actFuncDx = self._getActFuncDx(self.outType)
-        outputLayer = numLayers - 2
         delta = (self.targets - activations[outputLayer]) * actFuncDx(activations[outputLayer]) 
         deltas.update({outputLayer : delta})
         
@@ -118,8 +133,14 @@ class FFNN:
             actFuncDx = self._getActFuncDx(self.hiddenType)
             weights = self.weights[i+1]
             
-            # remember, we dont compute derivatives on bias nodes so we ignore those weights for now
-            delta = actFuncDx(activations[i]) * (np.dot(deltas[i+1], weights[:,1:]))
+            # we don't want to include the deltas for the bias in the next layer here so we omit it
+            # from the computation
+            if(i + 1 != outputLayer):
+                delta = deltas[i+1][:,1:]
+            else:
+                delta = deltas[i+1]
+
+            delta = actFuncDx(activations[i]) * (np.dot(delta, weights))
             deltas.update({i : delta})
             
             
@@ -134,17 +155,22 @@ class FFNN:
      
         # update input weights
         inputUpdate = np.zeros(self.weights[0].shape)
-        inputWithBias = np.insert(self.data, 0, 1, axis = 1)
-        inputUpdate = learnRate * np.dot(deltas[0].T, inputWithBias)
+        inputWithBias = np.insert(self.data, 0, self.bias, axis = 1)
+        inputUpdate = learnRate * np.dot(deltas[0][:,1:].T, inputWithBias)
         
         self.weights[0] += inputUpdate
         
         # now update the other weights
+        outputLayer = len(self.layers) - 2
         for i in range(len(self.layers) - 2):
-            act = activations[i]
-            act = np.insert(act, 0, 1, axis = 1)
+            # need to add that bias term back onto the activations
 
-            update = learnRate * np.dot(deltas[i+1].T, act)
+            if(i + 1 != outputLayer):
+                delta = deltas[i+1][:,1:]
+            else:
+                delta = deltas[i+1]
+
+            update = learnRate * np.dot(delta.T, activations[i])
             self.weights[i + 1] += update
         
         return
